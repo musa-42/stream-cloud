f7d6fadb9fb4910338dce9295216c509d9aa38d6
Your Name <you@example.com>
2021-10-30 10:39:52 +0330

    fix Permission denied

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./start.sh
+web: sh start.sh
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 gunicorn main:main --workers 4 --threads 4 --bind 0.0.0.0:$PORT --timeout 86400 --worker-class aiohttp.GunicornWebWorker & python -m bot

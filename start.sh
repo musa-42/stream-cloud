062da163b3b2ed946cd2edbcb825a47049a1d088
Musa Zadeh <83463048+musa-42@users.noreply.github.com>
2022-11-16 19:17:03 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn main:main --workers 4 --threads 4 --bind 0.0.0.0:$PORT --timeout 86400 --worker-class aiohttp.GunicornWebWorker & python -m bot
+gunicorn main:main --workers 4 --threads 4 --bind 0.0.0.0:8000 --timeout 86400 --worker-class aiohttp.GunicornWebWorker & python -m bot

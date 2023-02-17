f567a4c234d1e32fffbbb963f944e6555f53a5a7
Musa Zadeh <83463048+musa-42@users.noreply.github.com>
2023-02-17 10:04:05 +0330

    Update router.py
    Delete extra codes

--- api/router.py
+++ api/router.py
@@ -30,15 +30,7 @@ class Router:
         range_http = request.headers.get("Range", 0)
 
         if not isinstance(range_http, int):
-            #matches = self.RANGE_REGEX.search(range_http)
-            #if matches is None:
-            #    return web.HTTPBadRequest()
-            #offset = matches.group(1)
-            #end = matches.group(2) or file_size - 1
-            #if not offset.isdigit():
-            #    return web.HTTPBadRequest()
-            #offset = int(offset)
-            #end = int(end)
+
             offset, end = range_http.strip().strip('bytes=').split('-')
             if offset == "":
                 offset = 0
@@ -50,14 +42,11 @@ class Router:
             offset = 0
             end = file_size - 1
 
-        download_skip = (offset // self.BLOCK_SIZE) * self.BLOCK_SIZE
-        read_skip = offset - download_skip
 
-        if download_skip >= file_size:
+        if offset >= file_size:
             return web.HTTPRequestRangeNotSatisfiable()
 
-        if read_skip > self.BLOCK_SIZE:
-            return web.HTTPInternalServerError()
+
 
         if range_http:
             headers={
@@ -82,18 +71,13 @@ class Router:
         )
         await resp.prepare(request)
 
-        cls = self.client.iter_download(message.media, offset=download_skip, request_size=1024*1024 )
+        cls = self.client.iter_download(message.media, offset=offset, request_size=1024*1024 )
 
         async for part in cls:
             if offset > end:
                 break
-            if len(part) < read_skip:
-                read_skip -= len(part)
-            elif read_skip:
-                await resp.write(part[read_skip:])
-                read_skip = 0
-            else:
-                await resp.write(part)
+
+            await resp.write(part)
             offset += len(part)
             
         return resp
